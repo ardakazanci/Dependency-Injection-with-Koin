3d00ca928380533cf2a86a6fc4aa4b2be999ec6c
ardakazanci <ardakazanci@anadolu.edu.tr>
2020-03-23 15:15:30 +0300

    LoadingState Helper Class Added

---
+++ app/src/main/java/com/ardakazanci/dependencyinjectionwithkoin/viewmodel/UserViewModel.kt
@@ -0,0 +1,11 @@
+package com.ardakazanci.dependencyinjectionwithkoin.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.ardakazanci.dependencyinjectionwithkoin.repository.UserRepository
+
+class UserViewModel(private val repository : UserRepository) : ViewModel(){
+
+
+
+
+}
