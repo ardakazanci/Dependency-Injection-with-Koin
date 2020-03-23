b4800951fd25952af32ae19c5dce2958e305ecc1
ardakazanci <ardakazanci@anadolu.edu.tr>
2020-03-23 15:21:00 +0300

    UserViewModel Added

--- app/src/main/java/com/ardakazanci/dependencyinjectionwithkoin/utils/LoadingState.kt
+++ app/src/main/java/com/ardakazanci/dependencyinjectionwithkoin/utils/LoadingState.kt
@@ -1,15 +1,16 @@
 package com.ardakazanci.dependencyinjectionwithkoin.utils
 
+
+enum class Status {
+    RUNNING,
+    SUCCESS,
+    FAILED
+}
+
 data class LoadingState private constructor(val status: Status, val msg: String? = null) {
     companion object {
         val LOADED = LoadingState(Status.SUCCESS)
         val LOADING = LoadingState(Status.RUNNING)
         fun error(msg: String?) = LoadingState(Status.FAILED, msg)
-    }
-
-    enum class Status {
-        RUNNING,
-        SUCCESS,
-        FAILED
     }
 }
--- app/src/main/java/com/ardakazanci/dependencyinjectionwithkoin/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/ardakazanci/dependencyinjectionwithkoin/viewmodel/UserViewModel.kt
@@ -1,11 +1,46 @@
 package com.ardakazanci.dependencyinjectionwithkoin.viewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.ardakazanci.dependencyinjectionwithkoin.data.model.User
 import com.ardakazanci.dependencyinjectionwithkoin.repository.UserRepository
+import com.ardakazanci.dependencyinjectionwithkoin.utils.LoadingState
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class UserViewModel(private val repository : UserRepository) : ViewModel(){
+class UserViewModel(private val repository: UserRepository) : ViewModel(),Callback<List<User>> {
 
+    private val _loadingState = MutableLiveData<LoadingState>()
+    val loadingState: LiveData<LoadingState>
+        get() = _loadingState
 
+    private val _data = MutableLiveData<List<User>>()
+    val data: LiveData<List<User>>
+        get() = _data
+
+    init {
+        fetchData()
+    }
+
+    private fun fetchData(){
+        _loadingState.postValue(LoadingState.LOADING)
+        repository.getAllUsers().enqueue(this)
+    }
+
+    override fun onFailure(call: Call<List<User>>, t: Throwable) {
+        _loadingState.postValue(LoadingState.error(t.message))
+    }
+
+    override fun onResponse(call: Call<List<User>>, response: Response<List<User>>) {
+        if (response.isSuccessful) {
+            _data.postValue(response.body())
+            _loadingState.postValue(LoadingState.LOADED)
+        } else {
+            _loadingState.postValue(LoadingState.error(response.errorBody().toString()))
+        }
+    }
 
 
 }
